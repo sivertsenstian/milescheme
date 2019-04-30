d0c2635fd6411610212159c04635d3c91236f4d9
Stian Sivertsen <sivertsenstian@gmail.com>
2019-04-30 23:16:46 +0200

    === PART 1 ==== completed

--- msc.ts
+++ msc.ts
@@ -1,1 +1,8 @@
-export const tokenize = (chars: string): any => null;
+// Convert a string of characters into a list of tokens.
+export const tokenize = (chars: string): string[] => {
+  return chars
+    .replace(/\(/g, " ( ")
+    .replace(/\)/g, " ) ")
+    .split(" ")
+    .filter(t => t.length > 0);
+};
