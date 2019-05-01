fa01c1b6b8dd597ee940644755cdb236a2ef516b
Stian Sivertsen <sivertsenstian@gmail.com>
2019-05-01 12:24:00 +0200

    ====== PART 2 ===== completed

--- msc.ts
+++ msc.ts
@@ -10,10 +10,32 @@ export const tokenize = (chars: string): List => {
 };
 
 // Numbers become numbers; every other token is a symbol.
-export const atom = (token: string): any => null;
+export const atom = (token: string): Atom => {
+  const result = Number(token);
+  return isNaN(result) ? (token as MSCSymbol) : (result as MSCNumber);
+};
 
 // Read an expression from a sequence of tokens.
-export const readFromTokens = (tokens: List): any => null;
+export const readFromTokens = (tokens: List): Exp => {
+  if (tokens.length === 0) {
+    throw new SyntaxError("unexpected EOF");
+  }
+  let token = tokens.shift();
+  if (token === "(") {
+    let l : any[] = [];
+    while (tokens[0] !== ')') {
+      l.push(readFromTokens(tokens));
+    }
+    tokens.shift();
+    return l;
+  } else if (token === ")") {
+    throw new SyntaxError("unexpected token");
+  } else {
+    return atom(token as string);
+  }
+};
 
 // Read a Scheme expression from a string.
-export const parse = (program: string): any => null;
+export const parse = (program: string): Exp => {
+  return readFromTokens(tokenize(program));
+};
